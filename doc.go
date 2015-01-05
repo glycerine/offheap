0939ffcacb5dd976b09ab4c11002bc8662c75915
glycerine <j.e.aten@gmail.com>
2015-01-05 02:04:28 -0800

    atg. doc++

--- doc.go
+++ doc.go
@@ -17,41 +17,40 @@
     http://preshing.com/20130107/this-hash-table-is-faster-than-a-judy-array/
  for performance studies of the C++ code.
 
- ----------------------------------------------
 
-  HashTable
+HashTable
 
-  Maps pointer-sized integers to Cell structures, which in turn hold Val_t
+ Maps pointer-sized integers to Cell structures, which in turn hold Val_t
    as well as key_t structures.
 
-  Uses open addressing with linear probing. This makes it very cache
-   friendly and thus very fast.
+ Uses open addressing with linear probing. This makes it very cache
+  friendly and thus very fast.
 
-  In the t.Cells array, UnHashedKey = 0 is reserved to indicate an unused cell.
-  Actual value for key 0 (if any) is stored in t.ZeroCell.
-  The hash table automatically doubles in size when it becomes 75% full.
-  The hash table never shrinks in size, even after Clear(), unless you explicitly
-  call Compact().
+ In the t.Cells array, UnHashedKey = 0 is reserved to indicate an unused cell.
+ Actual value for key 0 (if any) is stored in t.ZeroCell.
+ The hash table automatically doubles in size when it becomes 75% full.
+ The hash table never shrinks in size, even after Clear(), unless you explicitly
+ call Compact().
 
  Basic operations: Lookup(), Insert(), DeleteKey(). These are the
  equivalent of the builtin map[uint64]interface{}.
 
-  As an example of how to specialize for a map[string]*Cell equivalent,
-   see the following functions in the bytekey.go file:
+ As an example of how to specialize for a map[string]*Cell equivalent,
+  see the following functions in the bytekey.go file:
 
- func (t *HashTable) InsertStringKey(strkey string, value interface{}) bool
- func (t *HashTable) LookupStringKey(strkey string) (Val_t, bool)
- func (t *HashTable) DeleteStringKey(strkey string) bool
+    func (t *HashTable) InsertStringKey(strkey string, value interface{}) bool
+    func (t *HashTable) LookupStringKey(strkey string) (Val_t, bool)
+    func (t *HashTable) DeleteStringKey(strkey string) bool
 
 
  Example use:
 
- h := offheap.NewHashTable(2)
+    h := offheap.NewHashTable(2)
 
- // basic three operations are:
- h.InsertStringKey("My number", 43)
- val, ok := h.LookupStringKey("My number")
- h.DeleteStringKey("My number")
+    // basic three operations are:
+    h.InsertStringKey("My number", 43)
+    val, ok := h.LookupStringKey("My number")
+    h.DeleteStringKey("My number")
 
 */
 package offheap
